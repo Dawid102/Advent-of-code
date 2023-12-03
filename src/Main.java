20047f0438e9720380203ff5625bd2ba83607e58
Dawid <dawidtro102@gmail.com>
2023-12-03 17:55:57 +0100

    Day 3 part 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,104 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
 //        day1();
-        day2();
+//        day2();
+        day3();
+    }
+
+    private static void day3() {
+        Scanner day3 = getScanner("input/day_3.txt");
+        char[][] schem = new char[140][139];
+        Set<Integer> toAdd = new HashSet<>();
+        ArrayList<List<Integer>> values = new ArrayList<>();
+        int i = 0;
+        int score = 0;
+        while (day3.hasNext()) {
+            var data = day3.next();
+            ArrayList<Integer> results = new ArrayList<>();
+            schem[i] = data.toCharArray();
+
+            for (int k = 0; k < data.length(); k++) {
+                int result = 0;
+                if (Character.isDigit(data.toCharArray()[k])) {
+                    for (int j = 0; j < 10; j++) {
+                        try {
+                            int digit = Integer.parseInt(String.valueOf(data.charAt(k+j)));
+//                            System.out.println(digit);
+                            schem[i][k+j] = Character.forDigit(results.size(), 16);
+                            result *= 10;
+                            result += digit;
+                        } catch (NumberFormatException a) {
+                            k += j-1;
+                            break;
+                        } catch (StringIndexOutOfBoundsException b) {
+                            break;
+                        }
+                    }
+                    results.add(result);
+//                    System.err.println(results.size());
+//                    System.out.println("res2 "+result);
+                } else {
+//                    System.out.println("Not digit"+data.toCharArray()[k]+"k: "+k);
+                }
+            }
+            values.add(results.stream().toList());
+
+            i++;
+        }
+        for (int k = 0; k < schem.length; k++) {
+            char[] chars = schem[k];
+            for (int l = 0; l < chars.length; l++) {
+                char ch = chars[l];
+                if (Character.isDigit(ch) || ch == '.' || Character.isAlphabetic(ch)) {
+                    continue;
+                } else {
+                    System.out.print(ch);
+                    if (k > 0 && l > 0) {
+                        char leftTop = schem[k-1][l-1];
+                        char top = schem[k-1][l];
+                        char rightTop = schem[k-1][l+1];
+                        char left = schem[k][l-1];
+                        char right = schem[k][l+1];
+                        char leftBottom = schem[k+1][l-1];
+                        char bottom = schem[k+1][l];
+                        char rightBottom = schem[k+1][l+1];
+                        if (isHex(leftTop)) {toAdd.add(getValue(leftTop)+16*(k-1));}
+                        if (isHex(top)) {toAdd.add(getValue(top)+16*(k-1));}
+                        if (isHex(rightTop)) {toAdd.add(getValue(rightTop)+16*(k-1));}
+                        if (isHex(left)) {toAdd.add(getValue(left)+16*k);}
+                        if (isHex(right)) {toAdd.add(getValue(right)+16*k);}
+                        if (isHex(leftBottom)) {toAdd.add(getValue(leftBottom)+16*(k+1));}
+                        if (isHex(bottom)) {toAdd.add(getValue(bottom)+16*(k+1));}
+                        if (isHex(rightBottom)) {toAdd.add(getValue(rightBottom)+16*(k+1));}
+                    }
+                }
+            }
+        }
+        System.out.println(toAdd);
+        for (int in : toAdd) {
+            int i1 = in % 16;
+            int i2 = (in - i1) / 16;
+            try {
+                score += values.get(i2).get(i1);
+            } catch (Exception e) {
+//                System.err.println(e);
+            }
+        }
+        System.out.println("Score "+score);
+
+//        System.out.println(values);
+//        System.out.println(Arrays.deepToString(schem));
+    }
+    private static boolean isHex(char ch) {
+        return Character.isDigit(ch) || Character.isAlphabetic(ch);
+    }
+    private static int getValue(char ch) {
+        return Integer.parseInt(String.valueOf(ch),16);
     }
 
     private static void day2() {
